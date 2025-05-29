a2467dd87af3affb47b052ea435a4c5f90bfa9fd
Harold Lomeli <haroldzapien@gmail.com>
2025-05-29 13:41:50 -0600

    modificacion en la forma de conextarse al puerto en el servidor

--- Server/index.js
+++ Server/index.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const app = require('./app');
 require("dotenv").config({ path: ".env" });
 
-const PORT = process.env.POST || 3997;
-const HOST = process.env.POST || "localhost";
+const PORT = process.env.PORT || 3997;
+mongoose.set('strictQuery', true); // elimina el warning de mongoose 7
 
 //se inicia la conexion con la base de datos y con el servidor de la aplicacion pasando las variables declaradas en ./constants.js
 async function main() {
@@ -11,10 +11,16 @@ async function main() {
       `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`
     );
   
-    app.listen(PORT, HOST, () => {
-      console.log(`http://${HOST}:${PORT}/api/${process.env.API_VERSION}`);
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
     });
   }
+
+  /**
+   * app.listen(PORT, HOST, () => {
+      console.log(`http://${HOST}:${PORT}/api/${process.env.API_VERSION}`);
+    });
+   */
   
   //ejecuta la funcion de arriba para establecer la conexion
   main().catch((err) => console.log(err));
